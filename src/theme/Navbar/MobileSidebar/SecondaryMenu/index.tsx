e98cbf6c9b3e4a4c3f9911bde5d711d04b629149
benjokoren-ow <ben.koren@overwolf.com>
2024-07-09 17:49:02 +0300

    mobile menu adjustments

--- src/theme/Navbar/MobileSidebar/SecondaryMenu/index.tsx
+++ src/theme/Navbar/MobileSidebar/SecondaryMenu/index.tsx
@@ -7,10 +7,13 @@ import Translate from '@docusaurus/Translate';
 function SecondaryMenuBackButton(props: ComponentProps<'button'>) {
   return (
     <button {...props} type="button" className="clean-btn navbar-sidebar__back">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.51762 11L12.52 5.3L11 4L4 12L11 20L12.52 18.7L7.5176 13H20V11H7.51762Z" fill="currentcolor"/>
+      </svg>
       <Translate
         id="theme.navbar.mobileSidebarSecondaryMenu.backButtonLabel"
         description="The label of the back button to return to main menu, inside the mobile navbar sidebar secondary menu (notably used to display the docs sidebar)">
-        ← Back to main menu
+        Back to main menu
       </Translate>
     </button>
   );
